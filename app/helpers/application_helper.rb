d6115e27ad5e61503a77e8b1359d334f7a752448
Vivek HV <hvvivek2007@gmail.com>
2017-12-26 16:19:40 -0500

    Added Heroku Changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,7 @@ module ApplicationHelper
       end
 
       def tag_cloud(tags, classes)
-        max = tags.sort_by(&:count).last
+        max = tags.last
         tags.each do |tag|
           index = tag.count.to_f / max.count * (classes.size - 1)
           yield(tag, classes[index.round])
