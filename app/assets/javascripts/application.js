6381572f39cfaeec21ae82fa8a790a08ad513998
Vivek HV <hvvivek2007@gmail.com>
2018-02-14 11:48:35 -0500

    Completed Contact Page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -441,9 +441,72 @@ $(document).on('turbolinks:load', function () {
 
 });
 
+PDFJS.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
+
+var loadPDF = function()
+{
+    
+    // If absolute URL from the remote server is provided, configure the CORS
+    // header on that server.
+    var url = 'https://s3.us-east-2.amazonaws.com/portfolio-vivek/documents/Resume.pdf';
+
+    // Disable workers to avoid yet another cross-origin issue (workers need
+    // the URL of the script to be loaded, and dynamically loading a cross-origin
+    // script does not work).
+    // PDFJS.disableWorker = true;
+
+    // The workerSrc property shall be specified.
+
+    // Asynchronous download of PDF
+    var loadingTask = PDFJS.getDocument(url);
+    loadingTask.promise.then(function(pdf) {
+    console.log('PDF loaded');
+    
+    // Fetch the first page
+    var pageNumber = 1;
+    pdf.getPage(pageNumber).then(function(page) {
+        console.log('Page loaded');
+        
+        var scale = 1.5;
+        var viewport = page.getViewport(scale);
+
+        // Prepare canvas using PDF page dimensions
+        var canvas = document.getElementById('the-canvas');
+        var context = canvas.getContext('2d');
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+
+        // Render PDF page into canvas context
+        var renderContext = {
+        canvasContext: context,
+        viewport: viewport
+        };
+        var renderTask = page.render(renderContext);
+        renderTask.then(function () {
+        console.log('Page rendered');
+        });
+    });
+    }, function (reason) {
+    // PDF loading error
+    console.error(reason);
+    });
+}
+
 setInterval(changeToNext, 3000)
 // setInterval(changeToNext2, 3000)
 
+function hideThis(e)
+{
+    $(e).css({"display":"none"})
+    console.log($(e).parent().find("video"))
+    $(e).parent().find("video").css({"display":"inline"})
+}
 
+function hideVideo(e)
+{
+    $(e).css({"display":"none"})
+    console.log($(e).parent().find("video"))
+    $(e).parent().find("img").css({"display":"inline"})
+}
 
 
